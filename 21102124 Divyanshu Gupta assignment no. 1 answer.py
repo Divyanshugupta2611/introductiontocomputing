714739922be847b44182e5935be110dde2999ec3
Divyanshugupta2611 <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-04-24 15:18:57 +0530

    Add files via upload

---
+++ 21102124 Divyanshu Gupta assignment no. 1 answer.py
@@ -0,0 +1,36 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Divyanshu\OneDrive\Desktop\assignment number 00001.py ===
+Answer of question number 1
+firstnumber=9
+second number=8
+third number=4
+Average= 7.0
+ 
+Answer of question number 2:
+Gross Income:950000
+no. of famiy members:5
+Tax= 187998.6
+ 
+Answer of question number 3
+Student
+SID:21102124
+Name:Divyanshu Gupta
+Gender:M
+Course Name:Introduction to python
+CGPA:8.5
+f [21102124, 'Divyanshu Gupta', 'M', 'Introduction to python', 8.5]
+ 
+Answer of question number 4
+Marks of 1st student:80
+Marks of 2nd student:86
+Marks of 3rd student:72
+Marks of 4th student:79
+Marks of 5th student:91
+sortedmarks [80, 86, 72, 79, 91]
+ 
+Answer of question number 5
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'pink', 'yellow']
+['red', 'green', 'white', 'puple', 'purple', 'yellow']
