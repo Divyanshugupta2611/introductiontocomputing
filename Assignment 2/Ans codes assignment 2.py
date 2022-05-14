30863801239d808a409af15e195da3eb8d2cf9ca
21102124 Divyanshu Gupta <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-05-14 19:48:59 +0530

    Add files via upload
    Divyanshu Gupta
    21102124
    Civil

---
+++ Assignment 2/Ans codes assignment 2.py
@@ -0,0 +1,38 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/Divyanshu/OneDrive/Desktop/python assignment 2 Q codes.py =
+Answer of Q.1..............................................
+Enter the string : Python is a case sensitive language
+Length of the given string:  35
+Reverse string:  egaugnal evitisnes esac a si nohtyP
+New string: a case sensitive
+New replaced string : Python is object oriented language
+Index of 'a': 10
+String without white spaces : Pythonisacasesensitivelanguage
+Answer of Q.2..............................................
+Enter your name :DIVYANSHU GUPTA
+Enter your sid :21102124
+Enter your department :CIVIL
+Enter your cgpa :8.9
+Hey, DIVYANSHU GUPTA here!
+My SID is 21102124
+I am from CIVIL department and my CGPA is 8.900000
+Answer of Q.3..............................................
+a&b : 8
+a|b : 58
+a^b : 50
+Left shift both a and b with 2 bits : a = %d, b = %d (224, 40)
+Right shift a with 2 bits and b with 4 bits : a = %d, b = %d (14, 0)
+Answer of Q.4..............................................
+Enter a string My name is Divyanshu Gupta.
+Yes
+Answer of Q.5...............................................
+10
+11
+8
+Yes
+Answer of Q.6...............................................
+Enter number a:12
+Enter number b:18
+4
