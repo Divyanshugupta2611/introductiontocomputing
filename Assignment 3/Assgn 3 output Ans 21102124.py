ee48308e40cddce6541f246b42001563cb463bb4
21102124 Divyanshu Gupta <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-06-09 21:13:30 +0530

    Add files via upload

---
+++ Assignment 3/Assgn 3 output Ans 21102124.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/Divyanshu/OneDrive/Desktop/Assign 3 inputs Q 21102124.py ==
+Enter a number to convert it into its binary equivalent:226
+ Its Binary value:
+011100010Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+1
+Enter first number: 25
+Enter second number: 11
+The sum is :36
+Enter the value of a :5
+Enter the value of b :9
+Enter the value of c :3
+Ans = 42
+Enter the value of n :5
+Ans = 10.0
+Ans = 4*math.pi*r**2
+Ans = math.sqrt(r*(math.cos(a))**2+r*(math.sin(a))**2
+Ans = y2-y1)/(x2-x1
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :
+How many hydrogen atoms? 4
+Carbon? 1
+Oxygen? 1
+The total molecular weight is 32.04186
