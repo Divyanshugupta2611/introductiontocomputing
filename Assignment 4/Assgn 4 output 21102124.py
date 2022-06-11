78fda4d0b8b2abb6b0d436e5fb174aae331c2a75
21102124 Divyanshu Gupta <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-06-11 22:18:48 +0530

    Add files via upload

---
+++ Assignment 4/Assgn 4 output 21102124.py
@@ -0,0 +1,33 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/Divyanshu/OneDrive/Desktop/assgn 4 codes 21102124.py ====
+ANS. 1
+Enter Marks :82
+ Grade A 
+ANS. 2
+Enter a year: 2020
+year is a Leap Year
+ANS. 3
+Question 1:1X5=5
+Right!
+Question 2:8X2=16
+Right!
+Question 3:2X2=7
+Wrong.The right answer is 4
+Question 4:2X3=6
+Right!
+Question 5:3X1=3
+Right!
+Question 6:9X8=86
+Wrong.The right answer is 72
+Question 7:8X2=15
+Wrong.The right answer is 16
+Question 8:5X10=50
+Right!
+Question 9:2X3=6
+Right!
+Question 10:3X6=6
+Wrong.The right answer is 18
+ANS. 4
+177 candies are in the bowl!
