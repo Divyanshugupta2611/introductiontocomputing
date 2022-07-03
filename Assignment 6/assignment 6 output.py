8f1bb734684ea21b28e175037a926774c606fe7d
21102124 Divyanshu Gupta <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-07-03 20:14:56 +0530

    Add files via upload

---
+++ Assignment 6/assignment 6 output.py
@@ -0,0 +1,26 @@
+True
+True
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+True
+enter the string: My-name-is-Abhishek-Narine
+Abhishek-My-Narine-is-name
+Student ID: 21102127
+Student Name: Abhishek_Narine
+Class: B.Tech_1st_year
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+[[-10, 2, 8], [-7, -3, 10]]
+True
+False
+True
+>>> 
