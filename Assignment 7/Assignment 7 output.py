e69dfe3cd8864ab1a01afc542c495c6ab7049b6d
21102124 Divyanshu Gupta <103355743+Divyanshugupta2611@users.noreply.github.com>
2022-07-16 21:55:19 +0530

    Add files via upload

---
+++ Assignment 7/Assignment 7 output.py
@@ -0,0 +1,31 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Divyanshu/AppData/Local/Programs/Python/Python310/jgjgu.py =
+Enter size of list: 5
+Enter marks: 55
+Enter marks: 68
+Enter marks: 95
+Enter marks: 15
+Enter marks: 35
+[15, 35, 55, 68, 95]
+Enter size of array: 8
+Enter element 26
+Enter element 95
+Enter element 77
+Enter element 66
+Enter element 11
+Enter element 56
+Enter element 73
+Enter element 19
+[11, 19, 26, 56, 66, 73, 77, 95]
+Enter element to be searched and counted: 7
+Error! Element is not present.
+Number of occurrences of element is 0
+Enter number of inputs:5
+Enter number:15
+Enter number:23
+Enter number:94
+Enter number:75
+Enter number:4
+[4, 15, 23, 75, 94]
